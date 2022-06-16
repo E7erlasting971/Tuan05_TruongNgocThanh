7b05cc47926588bc9b56c01866ff6cc8938e4637
E7erlasting971 <91707431+E7erlasting971@users.noreply.github.com>
2022-06-16 14:18:16 +0700

    thanh

--- Tuan05_TruongNgocThanh/App_Start/RouteConfig.cs
+++ Tuan05_TruongNgocThanh/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tuan05_TruongNgocThanh
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Theloai", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
