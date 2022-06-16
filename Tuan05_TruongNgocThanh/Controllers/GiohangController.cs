e033ebc62459b103f4c06bfd8218ab80b129310d
E7erlasting971 <91707431+E7erlasting971@users.noreply.github.com>
2022-06-16 16:25:42 +0700

    thanh

---
+++ Tuan05_TruongNgocThanh/Controllers/GiohangController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan05_TruongNgocThanh.Models;
+
+namespace Tuan05_TruongNgocThanh.Controllers
+{
+    public class GiohangController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        // GET: Giohang
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang == null)
+            {
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        public ActionResult ThemGioHang(int id, string strURL)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.Find(n => n.masach == id);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(id);
+                lstGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+    }
+}
